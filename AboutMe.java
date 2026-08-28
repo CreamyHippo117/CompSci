52d5fc2cb3a9a70d23bdb8609ebe74981c877f5f
CreamyHippo117 <atlasamiri4@gmail.com>
2026-08-28 14:42:34 -0700

    Day 1

---
+++ AboutMe.java
@@ -0,0 +1,9 @@
+public class AboutMe {
+    public static void main(String[] args) {
+        System.out.println("Name: Atlas");
+        System.out.println("Birthday: October 8, 2010");
+        System.out.println("Hobbies: Surf, Video Games, Math");
+        System.out.println("Favorite Book: none");
+        System.out.println("Favorite Movie: Inglorious Bastards");
+    }
+}
--- BlockLetters.java
+++ BlockLetters.java
@@ -1,15 +1,17 @@
 public class BlockLetters {
     public static void main(String[] args) {
-        System.out.println("    A   ");
-        System.out.println("   A  A  ");
-        System.out.println("  A    A ");
-        System.out.println(" A A A A A ");
-        System.out.println("A         A   ");
+        System.out.println("     A   ");
+        System.out.println("    A A  ");
+        System.out.println("   A   A ");
+        System.out.println("  A  A  A ");
+        System.out.println(" A       A");
+        System.out.println("A         A");
 
-        System.out.println("    A   ");
-        System.out.println("   A  A  ");
-        System.out.println("  A    A ");
-        System.out.println(" A A A A A ");
-        System.out.println("A         A   ");
+        System.out.println("     A   ");
+        System.out.println("    A A  ");
+        System.out.println("   A   A ");
+        System.out.println("  A  A  A ");
+        System.out.println(" A       A");
+        System.out.println("A         A");
     }
 }
