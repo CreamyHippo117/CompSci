2e2775ad2c48c542ac20c4c850cab754b1486076
CreamyHippo117 <atlasamiri4@gmail.com>
2026-09-01 12:51:57 -0700

    Update Unit0Exercises.java

--- Unit0Exercises.java
+++ Unit0Exercises.java
@@ -1,18 +1,18 @@
 
 
-public class Unit0Excercises {
+public class Unit0Exercises {
     public static void main(String[] args) {
         int score = 85;
         double price = 19.99;
         boolean isPassing = true;
-        System.out.println("---Variable Snapshot---");
+        System.out.println("--- Variable Snapshot ---");
         System.out.println("Score: " + score);
         System.out.println("Price: " + price);
         System.out.println("Passing: " + isPassing);
         score = score + 5;
         price = price + 2.5;
         isPassing = false;
-        System.out.println("---After Updates---");
+        System.out.println("--- After Updates ---");
         System.out.println("Score: " + score);
         System.out.println("Price: " + price);
         System.out.println("Passing: " + isPassing);
